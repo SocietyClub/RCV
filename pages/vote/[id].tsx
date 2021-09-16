f67409e81e2df7f36623e7fb9d8489cad8e0200d
Binam Kayastha <binamkayastha@gmail.com>
2021-09-15 22:47:13 -0400

    Add ViewPoll component
    Add generic layout.

--- pages/vote/[id].tsx
+++ pages/vote/[id].tsx
@@ -1,22 +1,25 @@
 import { useRouter } from 'next/router'
 import React from 'react'
+import ViewPoll from '../../components/ViewPoll'
 
 
 function VotePage() {
   const router = useRouter()
   const { id } = router.query
   // useEffect, get the voting question and candidates by id
+  const question = "What Movie Should We Watch?"
+  const pollCandidates = [
+	  "Superman",
+	  "Spiderman",
+	  "Batman",
+	  "Avengers",
+	  "Joker",
+	  "Captain Marvel"
+  ]
+
 
   return (
-	<div>
-	  <h1>Vote</h1>
-	  <p>
-		This is the page for voting on {id}.
-	  </p>
-	  <p>
-		<a href="/">Go to the home page</a>
-	  </p>
-	</div>
+	  <ViewPoll pollQuestion={question} pollCandidates={pollCandidates} />
   )
 }
 
