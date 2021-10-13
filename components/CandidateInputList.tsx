7ee71f671c41ca604c7023708d93d1359bc4c216
Cory Tapply <coryt0121@gmail.com>
2021-10-13 16:56:38 -0400

    Added the update poll page with refactored poll input components for both the create and update pages to use

--- components/CandidateInputList.tsx
+++ components/CandidateInputList.tsx
@@ -1,47 +1,79 @@
-import { Button, TextField, Typography } from '@mui/material';
-import React, { useState, useEffect, useCallback } from 'react';
-import styles from './CandidateInputList.module.css';
+import { Button, TextField, Typography } from "@mui/material";
+import React, { useState, useEffect, useCallback } from "react";
+import styles from "./CandidateInputList.module.css";
 
 type Props = {
-	textFieldClassName: string,
+  textFieldClassName: string;
+  candidateList: Array<Object>;
+  setCandidateList: (candidates: Array<Object>) => void;
 } & typeof defaultProps;
 
-const defaultProps = {}
+const defaultProps = {};
 
 const CandidateInputList = (props: Props) => {
-
-  const [numCandidates, setNumCandidates] = useState(1);
+  // Using this state to cause the focus to move to the new candidate input field
+  // When the Enter Key or Add Candidate button is pressed, I didn't want to listen to
+  // props.candidateList changes since those can happen from API calls
+  // and we don't always want to change focus when that happens
+  const [focusChange, setNumFocusChange] = useState(true);
 
   const addCandidate = () => {
-    setNumCandidates(numCandidates + 1);
-  }
+    setNumFocusChange(!focusChange);
+    props.setCandidateList([...props.candidateList, { name: "" }]);
+  };
 
   const setFocusToLastCandidate = useCallback(() => {
-    const lastCandidateTextInput = document.querySelectorAll('.' + props.textFieldClassName + ':last-of-type input')[0];
+    const lastCandidateTextInput = document.querySelectorAll(
+      "." + props.textFieldClassName + ":last-of-type input"
+    )[0];
     if (lastCandidateTextInput) {
       lastCandidateTextInput.focus();
     }
   }, [props.textFieldClassName]);
 
-  const handleKeyPress = (event: React.KeyboardEvent) => {
-    if(event.key === 'Enter'){
-      setNumCandidates(numCandidates + 1);
+  const handleEnterKey = (event: React.KeyboardEvent) => {
+    if (event.key === "Enter") {
+      setNumFocusChange(!focusChange);
+      props.setCandidateList([...props.candidateList, { name: "" }]);
     }
-  }
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const index: number = Number(event.target.name);
+    const candidateListCopy = [...props.candidateList];
+    candidateListCopy[index] = { name: event.target.value };
+    props.setCandidateList(candidateListCopy);
+  };
 
   useEffect(() => {
     setFocusToLastCandidate();
-  }, [numCandidates, setFocusToLastCandidate]);
+  }, [focusChange, setFocusToLastCandidate]);
 
   return (
     <>
       <Typography variant="h4">Candidates</Typography>
-      {Array(numCandidates).fill(0).map((_,i) => 
-        <TextField key={i} className={props.textFieldClassName} onKeyPress={handleKeyPress} label="Candidate Name" variant="filled" />
-      )}
-      <Button className={styles.addCandidateButton} variant="contained" onClick={addCandidate}>Add Candidate</Button>
+      {props.candidateList.map((candidate, i) => (
+        <TextField
+          key={i}
+          name={String(i)}
+          className={props.textFieldClassName}
+          onKeyPress={handleEnterKey}
+          label="Candidate Name"
+          variant="filled"
+          value={candidate.name}
+          onChange={handleChange}
+        />
+      ))}
+
+      <Button
+        className={styles.addCandidateButton}
+        variant="contained"
+        onClick={addCandidate}
+      >
+        Add Candidate
+      </Button>
     </>
-  )
-}
+  );
+};
 
 export default CandidateInputList;
--- components/api.ts
+++ components/api.ts
@@ -1,10 +1,42 @@
 export const CreatePollRequest = (data: Object) => {
-  return fetch('http://localhost:8080/ranked-choice-vote/v1/poll', { 
-    method: 'POST',
-    mode: 'no-cors', // TODO: Too lazy to figure out cors right now :(
+  return fetch("http://localhost:8080/ranked-choice-vote/v1/poll", {
+    method: "POST",
+    mode: "cors",
     headers: {
-      'Content-Type': 'application/json',
-      'X-USER-ID': '12345' // TODO: Hook up the unique ID for the users here when we start generating those
+      "Content-Type": "application/json",
+      "X-USER-ID": "12345", // TODO: Hook up the unique ID for the users here when we start generating those
     },
-    body: JSON.stringify(data)})
-}
+    body: JSON.stringify(data),
+  }).then((data) => data.json());
+};
+
+export const UpdatePollRequest = (id: String, data: Object) => {
+  return fetch(`http://localhost:8080/ranked-choice-vote/v1/poll/${id}`, {
+    method: "PATCH",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+      "X-USER-ID": "12345", // TODO: Hook up the unique ID for the users here when we start generating those
+    },
+    body: JSON.stringify(data),
+  }).then((data) => data.json());
+};
+
+export const GetPollRequest = (id: String) => {
+  return fetch(`http://localhost:8080/ranked-choice-vote/v1/poll/${id}`, {
+    method: "GET",
+    mode: "cors",
+    headers: {
+      Accept: "*/*",
+      "Content-Type": "application/json",
+      "X-USER-ID": "12345", // TODO: Hook up the unique ID for the users here when we start generating those
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response;
+    })
+    .then((data) => data.json())
+};
