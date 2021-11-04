1eda3255826c53d6d281f4af5a674b676c44f8c4
binamkayastha <binamkayastha@gmail.com>
2021-11-03 20:13:14 -0400

    Fixup use default prettier spacing of 2

--- components/CandidateSelectionBox.tsx
+++ components/CandidateSelectionBox.tsx
@@ -3,35 +3,35 @@ import MenuIcon from "@mui/icons-material/Menu";
 import Box from "@mui/material/Box";
 
 type Props = {
-    candidateName: string;
+  candidateName: string;
 };
 
 function CandidateSelectionBox({ candidateName }: Props) {
-    return (
-        <Box
-            style={{
-                width: "360px",
-                height: "56px",
-                marginBottom: "1rem",
-                padding: "0px 20px",
+  return (
+    <Box
+      style={{
+        width: "360px",
+        height: "56px",
+        marginBottom: "1rem",
+        padding: "0px 20px",
 
-                display: "flex",
-                flexDirection: "row",
-                alignItems: "center",
-                justifyContent: "space-between",
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
 
-                fontSize: "24px",
-                lineHeight: "24px",
-                fontFamily: "Roboto",
-            }}
-            sx={{
-                boxShadow: 3,
-            }}
-        >
-            {candidateName}
-            <MenuIcon />
-        </Box>
-    );
+        fontSize: "24px",
+        lineHeight: "24px",
+        fontFamily: "Roboto",
+      }}
+      sx={{
+        boxShadow: 3,
+      }}
+    >
+      {candidateName}
+      <MenuIcon />
+    </Box>
+  );
 }
 
 export default CandidateSelectionBox;
--- pages/vote/[id].tsx
+++ pages/vote/[id].tsx
@@ -3,43 +3,41 @@ import React, { useState, useEffect } from "react";
 import ViewPoll from "../../components/ViewPoll";
 
 function VotePage() {
-    const router = useRouter();
-    const question = "What Movie Should We Watch?";
-    type Hello = {
-        foo: string;
-        bar: string;
+  const router = useRouter();
+  const question = "What Movie Should We Watch?";
+  type Hello = {
+    foo: string;
+    bar: string;
+  };
+  const aVar: Hello = {
+    foo: "foo",
+    bar: "bar",
+  };
+
+  const [pollCandidates, setPollCandidates] = useState([]);
+
+  useEffect(() => {
+    if (!router.isReady) {
+      return;
+    }
+
+    const populatePollCandidates = async () => {
+      const { id } = router.query;
+      // TODO: Replace with Cory's function to also pass in the user UUID
+      const getPollUrl =
+        "https://societyclub-rcv-backend.uc.r.appspot.com/ranked-choice-vote/v1/poll/" +
+        id;
+      const pollResponse: any = await fetch(getPollUrl);
+      const pollResponseJson: any = await pollResponse.json();
+      setPollCandidates(
+        pollResponseJson.candidateList.map((x: { name: string }) => x.name)
+      );
     };
-    const aVar: Hello = {
-        foo: "foo",
-        bar: "bar",
-    };
 
-    const [pollCandidates, setPollCandidates] = useState([]);
+    populatePollCandidates();
+  }, [router.isReady]);
 
-    useEffect(() => {
-        if (!router.isReady) {
-            return;
-        }
-
-        const populatePollCandidates = async () => {
-            const { id } = router.query;
-            // TODO: Replace with Cory's function to also pass in the user UUID
-            const getPollUrl =
-                "https://societyclub-rcv-backend.uc.r.appspot.com/ranked-choice-vote/v1/poll/" +
-                id;
-            const pollResponse: any = await fetch(getPollUrl);
-            const pollResponseJson: any = await pollResponse.json();
-            setPollCandidates(
-                pollResponseJson.candidateList.map(
-                    (x: { name: string }) => x.name
-                )
-            );
-        };
-
-        populatePollCandidates();
-    }, [router.isReady]);
-
-    return <ViewPoll pollQuestion={question} pollCandidates={pollCandidates} />;
+  return <ViewPoll pollQuestion={question} pollCandidates={pollCandidates} />;
 }
 
 export default VotePage;
