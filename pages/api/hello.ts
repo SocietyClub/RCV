ef02a930e80168ff6b2b5b58a8a7da87cb348e59
binamkayastha <binamkayastha@gmail.com>
2022-06-14 00:59:24 -0400

    Add example firebase query to hello.ts api

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -2,12 +2,39 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+  name: string,
+  data: any
 }
 
-export default function handler(
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+
+
+const serviceAccount = require('./firestore_prod_key.json');
+
+
+initializeApp({
+credential: cert(serviceAccount)
+});
+
+const db = getFirestore();
+
+
+
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  const doc = await db.collection('polls').doc('test-do-not-deleto').get();
+
+  res.status(200).json({
+      name: 'John Does Not',
+      data: doc.data()
+  })
+}
+
+/*
+export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
 }
+*/
