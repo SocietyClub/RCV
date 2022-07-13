5a2a14fb87f2b1a46a65bc40d482f7a9d6b2f796
binamkayastha <binamkayastha@gmail.com>
2022-07-13 19:54:04 -0400

    Refactor [pollId] to make PATCH method path easier
    No changes to GET method logic

--- pages/api/v1/poll/[pollId].ts
+++ pages/api/v1/poll/[pollId].ts
@@ -1,12 +1,10 @@
 import admin from 'firebase-admin';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { Buffer } from 'buffer';
-import { getFirestore, Timestamp, FieldValue } from 'firebase-admin/firestore';
-import { initializeApp, applicationDefault, cert } from 'firebase-admin/app';
-
-type Data = {
-  data: any;
-};
+import { Severity } from '../../../../components/models/Enums';
+import { cert } from 'firebase-admin/app';
+import { createMessage } from '../../../../components/utils/utils';
+import { getFirestore } from 'firebase-admin/firestore';
 
 const serviceAccount = JSON.parse(Buffer.from(process.env.FIRESTORE_KEY_BASE64 || '', 'base64').toString('utf-8'));
 
@@ -18,14 +16,39 @@ if (admin.apps.length === 0) {
 
 const db = getFirestore();
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse<ServerResponse>) {
   const { pollId } = req.query;
-  const doc = await db
-    .collection('polls')
-    .doc(pollId as string)
-    .get();
 
-  res.status(200).json({
-    data: doc.data(),
+  if(req.method === 'GET') {
+    const doc = await db
+      .collection('polls')
+      .doc(pollId as string)
+      .get();
+
+    const pollData = doc.data()
+
+    if (!pollData) {
+      return res.status(500).json({
+        status: "Error",
+        messages: [
+          createMessage(Severity.ERROR, "Database Call Failed", "No data returned for that pollId")
+        ]
+      });
+
+    }
+
+    res.status(200).json({
+      data: pollData,
+    });
+  }
+  else if (req.method === 'PATCH') {
+  }
+
+  // Request method didn't match checks above
+  return res.status(400).json({
+    status: "Error",
+    messages: [
+      createMessage(Severity.ERROR, "Request Method Issue", "Request must be of type GET or PATCH for this endpoint")
+    ]
   });
 }
