3ab3e2b75c5c2453c52382dcc37a15d9ce43ac2b
binamkayastha <binamkayastha@gmail.com>
2022-11-06 00:10:53 -0400

    Add FormModalEndOfResultsButton to results page
    Add FormModalEndOfResults Button and Modal
    Create shared FormModal styles
    Some autoformatting and import re-ordering

--- components/FormModalEndOfResults.tsx
+++ components/FormModalEndOfResults.tsx
@@ -3,54 +3,128 @@ import { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Modal from '@mui/material/Modal';
+import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 
 import { useUserID } from '../hooks/useUserID';
-import styles from './FormModalFirstVisit.module.css';
+import styles from './FormModal.module.css';
 
-export function FormModalEndOfResult() {
-  const [firstFormStatus, setFirstFormStatus] = useState<string>();
+export type FormStatus = 'not_filled' | 'filled';
+
+type Props = {
+  open: boolean;
+  onClose: () => void;
+  endFormStatus: FormStatus
+  setEndFormStatus: (formStatus: FormStatus) => void;
+};
+
+export function FormModalEndOfResult({ open, onClose, endFormStatus, setEndFormStatus }: Props) {
+  const [understandingResponse, setUnderstandingResponse] = useState<number>();
+  const [npsResponse, setNpsResponse] = useState<number>();
+  const [feedbackResponse, setFeedbackResponse] = useState<string>('');
+  const [emailResponse, setEmailResponse] = useState<string>('');
   const userID = useUserID();
 
-  useEffect(() => {
-    setFirstFormStatus(localStorage.getItem('firstFormStatus') || 'not_filled');
-  }, [setFirstFormStatus]);
-
-  if (firstFormStatus === undefined || firstFormStatus === 'filled') {
-      return null;
+  if (endFormStatus === undefined || endFormStatus === 'filled' || open === false) {
+    return null;
   }
 
-  async function onSubmit(understandingResponse: number) {
-    if (understandingResponse === undefined || !(0 <= understandingResponse && understandingResponse <= 3)) {
-      alert(`Response is required`);
+  async function onSubmit() {
+    const noUnderstandingResponse = understandingResponse === undefined || !(0 <= understandingResponse && understandingResponse <= 3)
+    const noRecommendationResponse = npsResponse === undefined || !(0 <= npsResponse && npsResponse <= 10)
+    if (noUnderstandingResponse || noRecommendationResponse) {
+      if (noUnderstandingResponse && noRecommendationResponse) {
+          alert(`Understanding response and recommendation response are required`);
+      } else if (noUnderstandingResponse) {
+          alert(`Understanding response is required`);
+      } else if (noRecommendationResponse) {
+          alert(`Recommendation response is required`);
+      }
       return;
     }
-    const formURL = `https://docs.google.com/forms/d/e/1FAIpQLSebLC3w9orn8keIR69CqgBtaR7i-fSLAaMZnUKyhcxZfn16IQ/formResponse?usp=pp_url&entry.627021736=${userID}&entry.675560448=${understandingResponse}&submit=Submit`
-    localStorage.setItem('firstFormStatus', 'filled');
-    setFirstFormStatus('filled');
+    // TODO: Create different staging and prod forms
+    const formURL = `https://docs.google.com/forms/d/e/1FAIpQLSf_ElYFRkokcozeVod1p6WMOW4-ZroxyQqhNz2M3xnqz8qzkQ/formResponse?usp=pp_url&entry.627021736=${userID}&entry.675560448=${understandingResponse}&entry.1077870871=${npsResponse}&entry.2039317476=${feedbackResponse}&entry.1229452016=${emailResponse}&submit=Submit`;
+    setEndFormStatus('filled');
     // We use no-cors here because google form doesn't give a proper response if the form is not being submitted
     // through the site, though the form still submits
     // https://stackoverflow.com/questions/44012261/getting-around-cors-with-embedded-google-forms
-    await fetch(formURL, {    mode: 'no-cors' })
+    await fetch(formURL, { mode: 'no-cors' });
   }
 
   return (
-    <Modal open={true} onClose={onSubmit} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
+    <Modal open={open} onClose={onClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
       <Box className={styles.modal}>
-        <Typography id="modal-modal-title" variant="h6" component="h2">
-          I understand Ranked Choice Voting:
-        </Typography>
-        <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'left', margin: '1rem' }}>
-          <Button onClick={()=>{onSubmit(0)}} variant="contained" sx={{ marginBottom: '0.5rem' }}>
-            Strongly Disagree
+        <Box>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            I understand Ranked Choice Voting:
+          </Typography>
+          <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'left', margin: '1rem' }}>
+            <Button onClick={() => setUnderstandingResponse(0)} variant={understandingResponse == 0 ? 'contained' : 'outlined'} sx={{ marginBottom: '0.5rem' }}>
+              Strongly Disagree
+            </Button>
+            <Button onClick={() => setUnderstandingResponse(1)} variant={understandingResponse == 1 ? 'contained' : 'outlined'} sx={{ marginBottom: '0.5rem' }}>
+              Disagree
+            </Button>
+            <Button onClick={() => setUnderstandingResponse(2)} variant={understandingResponse == 2 ? 'contained' : 'outlined'} sx={{ marginBottom: '0.5rem' }}>
+              Agree
+            </Button>
+            <Button onClick={() => setUnderstandingResponse(3)} variant={understandingResponse == 3 ? 'contained' : 'outlined'}>
+              Strongly Agree
+            </Button>
+          </Box>
+        </Box>
+
+        {/* Add Tab order */}
+        <Box>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            On a scale from 0 to 10, how likely are you to recommend this product to a friend or colleague?
+          </Typography>
+          <Box sx={{ display: 'flex', justifyContent: 'center', margin: '1rem', columnGap: '0.5rem' }}>
+            {[...Array.from(Array(11).keys())].map((num: number) => (
+              <Button onClick={() => setNpsResponse(num)} variant={npsResponse == num ? 'contained' : 'outlined'} className={styles.numberBox} key={num}>
+                {num}
+              </Button>
+            ))}
+          </Box>
+        </Box>
+
+        <Box>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Do you have any other feedback (Optional)?
+          </Typography>
+          <TextField
+            className={styles.textField}
+            id="outlined-basic"
+            label="Your feedback here."
+            variant="outlined"
+            multiline
+            rows={2}
+            onChange={e => setFeedbackResponse(e.target.value)}
+          />
+        </Box>
+
+        <Box>
+          <Typography className={styles.textField} id="modal-modal-title" variant="h6" component="h2">
+            Email address (Optional, subscribe to our future projects):
+          </Typography>
+          <TextField
+            className={styles.textField}
+            id="outlined-basic"
+            label="Email address."
+            variant="outlined"
+            onChange={e => setEmailResponse(e.target.value)}
+          />
+        </Box>
+
+        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+          <Button
+            variant={'contained'}
+            onClick={() => {
+              onSubmit();
+            }}
+          >
+            Submit
           </Button>
-          <Button onClick={()=>{onSubmit(1)}} variant="contained" sx={{ marginBottom: '0.5rem' }}>
-            Disagree
-          </Button>
-          <Button onClick={()=>{onSubmit(2)}} variant="contained" sx={{ marginBottom: '0.5rem' }}>
-            Agree
-          </Button>
-          <Button onClick={()=>{onSubmit(3)}} variant="contained">Strongly Agree</Button>
         </Box>
       </Box>
     </Modal>
--- components/FormModalFirstVisit.tsx
+++ components/FormModalFirstVisit.tsx
@@ -6,11 +6,11 @@ import Modal from '@mui/material/Modal';
 import Typography from '@mui/material/Typography';
 
 import { useUserID } from '../hooks/useUserID';
-import styles from './FormModalFirstVisit.module.css';
+import styles from './FormModal.module.css';
 
 export function FormModalFirstVisit() {
   const [firstFormStatus, setFirstFormStatus] = useState<string>();
-  const localStorageKey = 'endFormStatus';
+  const localStorageKey = 'firstFormStatus';
   const userID = useUserID();
 
   useEffect(() => {
@@ -18,7 +18,7 @@ export function FormModalFirstVisit() {
   }, [setFirstFormStatus]);
 
   if (firstFormStatus === undefined || firstFormStatus === 'filled') {
-      return null;
+    return null;
   }
 
   async function onSubmit(understandingResponse: number) {
@@ -26,13 +26,14 @@ export function FormModalFirstVisit() {
       alert(`Response is required`);
       return;
     }
-    const formURL = `https://docs.google.com/forms/d/e/1FAIpQLSebLC3w9orn8keIR69CqgBtaR7i-fSLAaMZnUKyhcxZfn16IQ/formResponse?usp=pp_url&entry.627021736=${userID}&entry.675560448=${understandingResponse}&submit=Submit`
+    // TODO: Create different staging and prod forms
+    const formURL = `https://docs.google.com/forms/d/e/1FAIpQLSebLC3w9orn8keIR69CqgBtaR7i-fSLAaMZnUKyhcxZfn16IQ/formResponse?usp=pp_url&entry.627021736=${userID}&entry.675560448=${understandingResponse}&submit=Submit`;
     localStorage.setItem(localStorageKey, 'filled');
     setFirstFormStatus('filled');
     // We use no-cors here because google form doesn't give a proper response if the form is not being submitted
     // through the site, though the form still submits
     // https://stackoverflow.com/questions/44012261/getting-around-cors-with-embedded-google-forms
-    await fetch(formURL, {    mode: 'no-cors' })
+    await fetch(formURL, { mode: 'no-cors' });
   }
 
   return (
@@ -42,16 +43,18 @@ export function FormModalFirstVisit() {
           I understand Ranked Choice Voting:
         </Typography>
         <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'left', margin: '1rem' }}>
-          <Button onClick={()=>{onSubmit(0)}} variant="contained" sx={{ marginBottom: '0.5rem' }}>
+          <Button onClick={() => onSubmit(0)} variant="contained" sx={{ marginBottom: '0.5rem' }}>
             Strongly Disagree
           </Button>
-          <Button onClick={()=>{onSubmit(1)}} variant="contained" sx={{ marginBottom: '0.5rem' }}>
+          <Button onClick={() => onSubmit(1)} variant="contained" sx={{ marginBottom: '0.5rem' }}>
             Disagree
           </Button>
-          <Button onClick={()=>{onSubmit(2)}} variant="contained" sx={{ marginBottom: '0.5rem' }}>
+          <Button onClick={() => onSubmit(2)} variant="contained" sx={{ marginBottom: '0.5rem' }}>
             Agree
           </Button>
-          <Button onClick={()=>{onSubmit(3)}} variant="contained">Strongly Agree</Button>
+          <Button onClick={() => onSubmit(3)} variant="contained">
+            Strongly Agree
+          </Button>
         </Box>
       </Box>
     </Modal>
